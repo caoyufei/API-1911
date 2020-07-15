64ad79d727c9a4b5b1c8973c1a5ded754e47b707
caoyufei <caoyufei@3183989523.com>
2020-07-15 12:00:37 +0800

    access_token

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,4 +10,13 @@ class TestController extends Controller
     {
         echo "hello world";
     }
+
+    public function getWxToken()
+    {
+        $appid='wx81daee32b142c95e';
+        $appsecret ='cb2ca80424b1bab9b20fb1edcfa59c33';
+        $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$appid.'&secret='.$appsecret;
+        $cont=file_get_contents($url);
+        echo $cont;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::get('/', function () {
 });
 
 Route::get('/hello',"TestController@hello");
+Route::get('/wx/token',"TestController@getWxToken");
